4cd1b80e731d3e26768c46b0945a8ef8415581a4
Samuel Du <Samuel Du@DESKTOP-1FO2DUK.home>
2021-04-18 17:36:29 -0400

    try run TwoCoinsChain

---
+++ src/com/two/coins/transactions/TransactionInput.java
@@ -0,0 +1,10 @@
+package com.two.coins.transactions;
+
+public class TransactionInput {
+	public String transactionOutputId; //Reference to TransactionOutputs -> transactionId
+	public TransactionOutput UTXO; //Contains the Unspent transaction output
+	
+	public TransactionInput(String transactionOutputId) {
+		this.transactionOutputId = transactionOutputId;
+	}
+}
